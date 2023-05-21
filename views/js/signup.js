7b4c01f668d5eb1f4fc0ed0ebea9cec80b5e2522
12210024 <12210024.gcit@rub.edu.bt>
2023-05-21 19:18:41 +0000

    update

--- views/js/signup.js
+++ views/js/signup.js
@@ -31,20 +31,3 @@ const sr = ScrollReveal ({
   sr.reveal('.footer-logo',{ delay:350, origin:'bottom' })
 
 
-
-// let menubar = document.querySelector('#menubar');
-// let mynav = document.querySelector('.navbar');
-
-
-
-// menubar.onclick = () =>{
-//     menubar.classList.toggle('fa-times')
-//     mynav.classList.toggle('active')
-// }
-
-// document.querySelector('#loginbtn').onclick = () =>{
-//     document.querySelector('.login-form').classList.toggle('active');
-//   }
-//   document.querySelector('#closeloginform').onclick = () =>{
-//     document.querySelector('.login-form').classList.remove('active');
-//   }
