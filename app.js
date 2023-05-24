0c6e5ad83b0e2d7ad4ed8cd929caea9a456ae127
12210024 <12210024.gcit@rub.edu.bt>
2023-05-24 04:42:20 +0000

    update

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const app = express()
 
 module.exports = app
 // const port = 4000
-const userRouter = require('./routes/userRoutes')
+const userRouter = require('./routes/userRoutes') //register the created route
 const viewRouter = require('./routes/viewRoutes')
 const path = require('path')
 
-app.use(express.json())
+app.use(express.json()) // middleware in the  application (req -> json -> res)
 
 app.use(express.static(__dirname+ "public"))
 
 
-app.use('/api/v1/users',userRouter)
+app.use('/api/v1/users',userRouter) //registering the created routes
 app.use('/', viewRouter)
 
 app.use(express.static(path.join(__dirname, 'views')))
